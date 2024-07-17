2a628748afff2f85006d466414b0b2eabbc62460
Evgeny Buchelnikov <gru554@yandex.ru>
2024-07-17 15:53:13 +1000

    added product and category entities

---
+++ services/product/src/main/java/com/evggenn/ecommerce/product/Product.java
@@ -0,0 +1,28 @@
+package com.evggenn.ecommerce.product;
+
+import com.evggenn.ecommerce.category.Category;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Setter
+@Getter
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String description;
+    private double availableQuantity;
+    private BigDecimal price;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+}
