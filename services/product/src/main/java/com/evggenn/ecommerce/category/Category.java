a04782a973cf142de13ddfd5ea1ccc2bc32a534e
Evgeny Buchelnikov <gru554@yandex.ru>
2024-07-17 16:29:11 +1000

    added sql init file for flyway versioning in product db

--- services/product/src/main/java/com/evggenn/ecommerce/category/Category.java
+++ services/product/src/main/java/com/evggenn/ecommerce/category/Category.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class Category {
     @Id
     @GeneratedValue
-    private Integer id;
+    private Long id;
     private String name;
     private String description;
     @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
