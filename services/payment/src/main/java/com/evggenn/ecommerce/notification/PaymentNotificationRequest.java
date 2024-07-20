f09fd2aeb9f8fb86c7553413948d0f96690f7143
Evgeny Buchelnikov <gru554@yandex.ru>
2024-07-20 10:28:24 +1000

    added kafka producer and topic for payment notification

---
+++ services/payment/src/main/java/com/evggenn/ecommerce/notification/PaymentNotificationRequest.java
@@ -0,0 +1,15 @@
+package com.evggenn.ecommerce.notification;
+
+import com.evggenn.ecommerce.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstname,
+        String customerLastname,
+        String customerEmail
+) {
+}
---
+++ services/payment/src/main/java/com/evggenn/ecommerce/payment/PaymentRequest.java
@@ -0,0 +1,13 @@
+package com.evggenn.ecommerce.payment;
+
+import java.math.BigDecimal;
+
+public record PaymentRequest(
+        Long id,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        Long orderId,
+        String orderReference,
+        Customer customer
+) {
+}
